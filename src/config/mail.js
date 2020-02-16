66a465605c64ff0b060b40c46c2a03bc2d7df9d4
Victor Hugo <viiict0r@hotmail.com>
2020-02-15 22:29:24 -0300

    chore: Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '60b7f7096de010',
+    pass: '8f428637004dea',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
